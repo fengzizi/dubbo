a52d567ddbfdc28ccbaf0c3ef9c780e59c9d1db2
fujian <854214434@qq.com>
2021-03-31 18:01:52 +0800

    #fix 配置中心

--- dubbo-user/dubbo-user-provider/src/main/java/com/fengzi/dubbo/user/service/impl/UserServiceImpl.java
+++ dubbo-user/dubbo-user-provider/src/main/java/com/fengzi/dubbo/user/service/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.fengzi.dubbo.user.service.impl;
 
+import com.alibaba.nacos.api.config.ConfigType;
+import com.alibaba.nacos.api.config.annotation.NacosConfigListener;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.fengzi.dubbo.user.service.UserService;
 import com.fengzi.dubbo.user.vo.UserVo;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -15,13 +20,20 @@ import java.net.UnknownHostException;
 @DubboService
 public class UserServiceImpl implements UserService {
 
+    @NacosValue(value = "${spring.redis.database}", autoRefreshed = true)
+    private Integer redisDatabase;
+
+    @NacosValue(value = "${spring.redis.host}", autoRefreshed = true)
+    private String redisHost;
+
+    @NacosValue(value = "${spring.redis.port}", autoRefreshed = true)
+    private Integer redisPort;
 
     @Override
     public UserVo findUserById(String id) {
         UserVo vo = new UserVo();
         vo.setUserId(id);
-        vo.setUserName(id + "的姓名");
-
+        vo.setUserName("redisDatabase=" + redisDatabase + ",redisHost=" + redisHost + ",redisPort=" + redisPort);
         InetAddress address;
         try {
             address = InetAddress.getLocalHost();
@@ -30,5 +42,15 @@ public class UserServiceImpl implements UserService {
             e.printStackTrace();
         }
         return vo;
+    }
+
+    @Component
+    @NacosPropertySource(dataId = "redis.yaml", autoRefreshed = true, type = ConfigType.YAML)
+    public class NacosListener {
+
+        @NacosConfigListener(dataId = "redis.yaml")
+        public void onMessage(String msg) {
+            System.out.println(msg);
+        }
     }
 }
